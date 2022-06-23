f3988134d113597ec6a4fcbe55157f78dd74f8b2
JackEdwards11 <jackmedwards@gmail.com>
2022-06-22 20:15:05 -0400

    Base

---
+++ dasher.cpp
@@ -0,0 +1,41 @@
+#include "raylib.h"
+
+int main()
+{
+    // Window dimesnions
+    const int windowWidth = 512;
+    const int windowHeight = 380;
+    // Initialize window
+    InitWindow(windowWidth, windowHeight, "Dapper Dasher");
+
+    // Rectangle dimesnions
+    const int width = 50;
+    const int height = 80;
+
+    int posY = windowHeight - height;
+    int velocity = -10;
+
+    // Set fps
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        // Logic
+        DrawRectangle(windowWidth/2, posY, width, height, BLUE);
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            posY += velocity;
+        }
+
+        // End Logic
+
+        EndDrawing();
+
+
+    }
+    CloseWindow();
+
+}
